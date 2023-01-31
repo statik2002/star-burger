57924da4f5631d256d0f924ccf0b18961077b260
statik2002 <statik2002@mail.ru>
2023-01-31 12:13:39 +0800

    Сделал возврат из админки на страницу с заказами

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.db import transaction
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
+from star_burger import settings
 from .models import Product, Order, OrderItem
 from .models import ProductCategory
 from .models import Restaurant
@@ -136,5 +140,18 @@ class OrderAdmin(admin.ModelAdmin):
             instance.price = instance.price
             instance.save()
         formset.save_m2m()
+
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+
+        if 'next' in request.GET and url_has_allowed_host_and_scheme(
+            request.GET['next'],
+            settings.ALLOWED_HOSTS,
+            require_https=False
+        ):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
+
 
 
--- restaurateur/views.py
+++ restaurateur/views.py
@@ -93,6 +93,8 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     orders = Order.objects.calc_order()
+    print(request.path)
     return render(request, template_name='order_items.html', context={
         'order_items': orders,
+        'path': request.path,
     })
