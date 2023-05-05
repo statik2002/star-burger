19ed603daf43fa70dd825edd409f55a071ca63ce
statik2002 <statik2002@mail.ru>
2023-05-05 15:41:48 +0800

    send message to RollBar after deploy success

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -7,8 +7,6 @@ set -e
 
 COMMIT_HASH=`git rev-parse HEAD`
 
-curl -H "X-Rollbar-Access-Token: 133ebaa4f3a34cebb5030d9b7e84a169" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "development", "revision": "'$COMMIT_HASH'", "rollbar_name": "statik2002", "local_username": "'$USER'", "comment": "intermediate deployment", "status": "succeeded"}'
-
 source env/bin/activate
 
 pip install -r requirements.txt
@@ -26,5 +24,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 sudo systemctl restart unit
+
+curl -H "X-Rollbar-Access-Token: 133ebaa4f3a34cebb5030d9b7e84a169" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "development", "revision": "'$COMMIT_HASH'", "rollbar_name": "statik2002", "local_username": "'$USER'", "comment": "intermediate deployment", "status": "succeeded"}'
 
 echo "All done!"
